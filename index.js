789916fa645e15a0d2ab74ccc8306a91a2ea58ba
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-17 16:10:21 -0600

    Make first pug version of pages

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const pug = require('pug');
 
 // Compile the source code
-const compiledFunction = pug.compileFile('pug_files/layout.pug');
+const compiledFunction = pug.compileFile('pug_files/home.pug');
 console.log(compiledFunction());
