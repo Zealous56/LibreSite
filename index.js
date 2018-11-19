36282641485bb627e9eede157cbd0878c14c52c2
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-18 19:34:00 -0600

    Standardize layout. Optimize JS loop

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 
 const pageList = ['blog', 'contact', 'index', 'info']
 
-for(let i = 0; i < pageList.length; i++) {
-    var page =  pageList[i];
+for(var i = 0; i < pageList.length; i++) {
+    let page =  pageList[i];
     var compiledFunction = pug.compileFile('pug_files/' + page + '.pug')
 
     fs.writeFile(page + '.html', compiledFunction(), (err) => {
         if(err) console.log(err)
-        else console.log('Compilation of ' + pageList[i] + ' successful!')
+        else console.log('Compilation of ' + page + ' successful!')
     })
 }
