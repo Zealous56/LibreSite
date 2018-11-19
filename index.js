4fa9f882071645f17b216e4d1bb1c026c9eee2f4
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-18 18:27:44 -0600

    Write compiled pug to file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const pug = require('pug');
+const fs = require('fs');
 
 // Compile the source code
 const compiledFunction = pug.compileFile('pug_files/home.pug');
-console.log(compiledFunction());
+fs.writeFile('index.html', compiledFunction(), (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Compilation successful!');
+    }
+});
