1d9e894c61986465a09df1c8b9bd6af2b3c04a0e
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-22 19:17:37 -0600

    Fix page linking

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const pageList = ['blog', 'contact', 'index', 'info']
 
 for(var i = 0; i < pageList.length; i++) {
-    let page =  pageList[i];
+    let page = pageList[i];
     var compiledFunction = pug.compileFile('pug_files/' + page + '.pug')
 
     fs.writeFile(page + '.html', compiledFunction(), (err) => {
