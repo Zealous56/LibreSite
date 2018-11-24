c616eb0af94378a006e12c9727acfb2b4ee0c63e
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-23 22:49:35 -0600

    Fix error checking code

--- index.js
+++ index.js
@@ -12,65 +12,64 @@ for(let i = 0; i < pageList.length; i++) {
     fs.readdir(path, (err, files) => {
         if(err) {
             console.log('Could not read directory ' + path)
-        } else {
-
-            /* Create arrays to store objects containing file contents */
-            var leftSidebar = []
-            var container = []
-            var rightSidebar = []
-
-            /* Iterate through all files in path */
-            files.forEach( (file) => {
-
-                /* Read file */
-                fs.readFile(path + '/' + file, 'utf8', (err, rawText) => {
-                    if(err) {
-                        console.log('Could not parse file ' + file)
-                    } else {
-
-                        /* Parse location and date for file (separated by underscore) */
-                        var fileName = file.split('_')
-                        var location = fileName[0]
-                        var date = fileName[1].replace('.txt', '')
-
-                        /* Parse header from text file (header is first line) */
-                        var header = rawText.split('\n', 1)[0]
-
-                        /* Parse content from text file (content is everything else) */
-                        var content = rawText.replace(header + '\n\n', '')
-
-                        /* Push data into corresponding array */
-                        switch(location) {
-                        case 'sidebar-left':
-                            leftSidebar.push(
-                                {
-                                    date: date,
-                                    header: header,
-                                    content: content
-                                }
-                            )
-                        case 'container':
-                            container.push(
-                                {
-                                    date: date,
-                                    header: header,
-                                    content: content
-                                }
-                            )
-                        case 'sidebar-right':
-                            rightSidebar.push(
-                                {
-                                    date: date,
-                                    header: header,
-                                    content: content
-                                }
-                            )
-                        }
-                        console.log('Successfully parsed file ' + path + '/' + file)
-                    }
-                })
-            })
         }
+
+        /* Create arrays to store objects containing file contents */
+        var leftSidebar = []
+        var container = []
+        var rightSidebar = []
+
+        /* Iterate through all files in path */
+        files.forEach( (file) => {
+
+            /* Read file */
+            fs.readFile(path + '/' + file, 'utf8', (err, rawText) => {
+                if(err) {
+                    console.log('Could not parse file ' + file)
+                    return;
+                }
+
+                /* Parse location and date for file (separated by underscore) */
+                var fileName = file.split('_')
+                var location = fileName[0]
+                var date = fileName[1].replace('.txt', '')
+
+                /* Parse header from text file (header is first line) */
+                var header = rawText.split('\n', 1)[0]
+
+                /* Parse content from text file (content is everything else) */
+                var content = rawText.replace(header + '\n\n', '')
+
+                /* Push data into corresponding array */
+                switch(location) {
+                case 'sidebar-left':
+                    leftSidebar.push(
+                        {
+                            date: date,
+                            header: header,
+                            content: content
+                        }
+                    )
+                case 'container':
+                    container.push(
+                        {
+                            date: date,
+                            header: header,
+                            content: content
+                        }
+                    )
+                case 'sidebar-right':
+                    rightSidebar.push(
+                        {
+                            date: date,
+                            header: header,
+                            content: content
+                        }
+                    )
+                }
+                console.log('Successfully parsed file ' + path + '/' + file)
+            })
+        })
     })
     
     /* Compile each page  */
@@ -80,8 +79,8 @@ for(let i = 0; i < pageList.length; i++) {
     fs.writeFile(page + '.html', compiledFunction(), (err) => {
         if(err) {
             console.log(err)
-        } else {
-            console.log('Compilation of ' + page + ' successful!')
+            return;
         }
+        console.log('Compilation of ' + page + ' successful!')
     })
 }
