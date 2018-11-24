f8ee2a6c7362572423b6cf0adb87a71bc7f4cd28
0Gitnick <36289298+0Gitnick@users.noreply.github.com>
2018-11-24 00:33:36 -0600

    Make writeFile synchronous

--- index.js
+++ index.js
@@ -9,73 +9,63 @@ for(let i = 0; i < pageList.length; i++) {
     let page = pageList[i];
     let path = contentPath + '/' + page
 
-    fs.readdir(path, (err, files) => {
-        if(err) {
-            console.log('Could not read directory ' + path)
-        }
+    var files = fs.readdirSync(path)
 
-        /* Create arrays to store objects containing file contents */
-        var leftSidebar = []
-        var container = []
-        var rightSidebar = []
+    /* Create arrays to store objects containing file contents */
+    var leftSidebar = []
+    var container = []
+    var rightSidebar = []
 
-        /* Iterate through all files in path */
-        files.forEach( (file) => {
+    /* Iterate through all files in path */
+    files.forEach( (file) => {
 
-            /* Read file */
-            var rawText = fs.readFileSync(path + '/' + file).toString()
+        /* Read file */
+        var rawText = fs.readFileSync(path + '/' + file).toString()
 
-            /* Parse location and date for file (separated by underscore) */
-            var fileName = file.split('_')
-            var location = fileName[0]
-            var date = fileName[1].replace('.txt', '')
+        /* Parse location and date for file (separated by underscore) */
+        var fileName = file.split('_')
+        var location = fileName[0]
+        var date = fileName[1].replace('.txt', '')
 
-            /* Parse header from text file (header is first line) */
-            var header = rawText.split('\n', 1)[0]
+        /* Parse header from text file (header is first line) */
+        var header = rawText.split('\n', 1)[0]
 
-            /* Parse content from text file (content is everything else) */
-            var content = rawText.replace(header + '\n\n', '')
+        /* Parse content from text file (content is everything else) */
+        var content = rawText.replace(header + '\n\n', '')
 
-            /* Push data into corresponding array */
-            switch(location) {
-            case 'sidebar-left':
-                leftSidebar.push(
-                    {
-                        date: date,
-                        header: header,
-                        content: content
-                    }
-                )
-            case 'container':
-                container.push(
-                    {
-                        date: date,
-                        header: header,
-                        content: content
-                    }
-                )
-            case 'sidebar-right':
-                rightSidebar.push(
-                    {
-                        date: date,
-                        header: header,
-                        content: content
-                    }
-                )
-            }
+        /* Push data into corresponding array */
+        switch(location) {
+        case 'sidebar-left':
+            leftSidebar.push(
+                {
+                    date: date,
+                    header: header,
+                    content: content
+                }
+            )
+        case 'container':
+            container.push(
+                {
+                    date: date,
+                    header: header,
+                    content: content
+                }
+            )
+        case 'sidebar-right':
+            rightSidebar.push(
+                {
+                    date: date,
+                    header: header,
+                    content: content
+                }
+            )
             console.log('Successfully parsed file ' + path + '/' + file)
-        })
+        }
     })
     
-    /* Compile each page  */
+    /* Compile each page to a file  */
     var compiledFunction = pug.compileFile('pug_files/' + page + '.pug', { pretty: true })
-
-    /* Write compiled page to a file */
-    fs.writeFile(page + '.html', compiledFunction(), (err) => {
-        if(err) {
-            console.log(err)
-            return;
-        }
-        console.log('Compilation of ' + page + ' successful!')
-    })
+    fs.writeFileSync(page + '.html', compiledFunction())
+    
+    console.log('Compilation of ' + page + ' successful!')
 }
